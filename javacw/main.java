a8fc28ad22f199ca86f767e9ec4a7b8d37548988
denis <31662599+denistepp1@users.noreply.github.com>
2018-04-06 02:10:54 -0700

    Create main.java

---
+++ javacw/main.java
@@ -0,0 +1,12 @@
+
+package wine;
+
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+
+public class main {
+      public static void main(String[] args) throws FileNotFoundException {
+         DataSet train = new DataSet("wine.txt");
+         //System.out.println(train.samples);
+    }
+}
