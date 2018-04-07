e3561ad6d9d83a7e3e076a93ad698ed0c46e367b
denis <31662599+denistepp1@users.noreply.github.com>
2018-04-07 03:30:07 -0700

    Update DataSet.java

--- javacw/DataSet.java
+++ javacw/DataSet.java
@@ -15,17 +15,30 @@ public final class DataSet {
         System.out.println("Calculating number of lines...");
         getDataSetSize(filename);
         DataSample[] dataArray = new DataSample[samples];
+        double[] attributes=new double[samples]; //to convert string array into doubles array
+
+        for(int i=0;i<samples;i++){
+            String[] token=sampleArr[i].split(","); //this is to split string by ','
+            label=Integer.parseInt(token[0]); //split label is token[0]
+
+            for(int j=1;j<token.length;j++)
+            attributes[j-1]=Double.parseDouble(token[j]); //token[1] to token[13] are attributes, this will convert string into //double
+
+            dataArray[i]=new DataSample(label,attributes); 
+            }
+        System.out.println(dataArray[0]);
     }
     
     public int getDataSetSize(String filename) throws FileNotFoundException {
+        samples=0;
         File file = new File(filename);
         try (Scanner input = new Scanner(file)) {
             while (input.hasNextLine()){
-                input.nextLine();
+                sampleArr[samples]=input.nextLine(); //sampleArr , has each line read from file
                 samples++;
             }
-        input.close();
-            System.out.println("done");
+            input.close();
+            System.out.println("done ");
         }
         return samples;
     }
